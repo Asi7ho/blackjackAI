7fce290dc7db39801d0545eae0df31da25fde020
Asi7ho <46624642+Asi7ho@users.noreply.github.com>
2021-01-13 14:48:25 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 
+from ai import Dqn
+import numpy as np
 from game import Game
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -10,6 +12,11 @@ WINDOW_WIDTH = 1136
 WINDOW_HEIGHT = 850
 CARD_WIDTH = 167
 CARD_HEIGHT = 225
+
+brain = Dqn(3, 2, 0.9)
+action = [1, 0]
+last_reward = 0
+scores = []
 
 buttonStyle = """
                 QPushButton {
@@ -38,6 +45,9 @@ class MainWindow(QMainWindow):
         self.username = "unknown"
         self.lineEdit = QLineEdit()
 
+        self.cardsDealt = False
+        self.exitGame = False
+
         # window
         self.setWindowTitle("Black Jack ++")
         self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -49,13 +59,13 @@ class MainWindow(QMainWindow):
 
     def homePage(self):
         # add buttons for home page
+        loadButton = QPushButton("Load")
+        loadButton.setStyleSheet(buttonStyle)
+        loadButton.clicked.connect(self.loadModel)
+
         rulesButton = QPushButton("Rules")
         rulesButton.setStyleSheet(buttonStyle)
         rulesButton.clicked.connect(self.rulesPage)
-
-        scoreButton = QPushButton("Scores")
-        scoreButton.setStyleSheet(buttonStyle)
-        scoreButton.clicked.connect(self.scoresPage)
 
         # layout
         hbox = QHBoxLayout()
@@ -65,8 +75,8 @@ class MainWindow(QMainWindow):
         vbox.addStretch(1)
         vbox.addLayout(hbox)
 
+        hbox.addWidget(loadButton)
         hbox.addWidget(rulesButton)
-        hbox.addWidget(scoreButton)
 
         widget = QWidget()
 
@@ -83,7 +93,7 @@ class MainWindow(QMainWindow):
         # add buttons for rules page
         playButton = QPushButton("Play")
         playButton.setStyleSheet(buttonStyle)
-        playButton.clicked.connect(self.processGame)
+        playButton.clicked.connect(self.handleGame)
 
         backButton = QPushButton("Back")
         backButton.setStyleSheet(buttonStyle)
@@ -113,64 +123,13 @@ class MainWindow(QMainWindow):
 
         self.game.initializeGameScore()
 
-    def scoresPage(self):
-        # add buttons for scores page
-        backButton = QPushButton("Back")
-        backButton.setStyleSheet(buttonStyle)
-        backButton.clicked.connect(self.homePage)
-
-        # layout
-        hbox = QHBoxLayout()
-        vbox = QVBoxLayout()
-
-        hbox.addStretch(1)
-
-        # add widgets
-        wrapperScoreV = QVBoxLayout()
-        wrapperScoreV.setContentsMargins(20, 120, 0, 0)
-        _, keys, values = self.game.getScores()
-        numScoresToPlot = min(15, len(keys))
-        for rank in range(numScoresToPlot):
-            wrapperScoreH = QHBoxLayout()
-            labelName = QLabel("{}".format(keys[rank]))
-            labelName.setFont(QFont('Arial', 26))
-            labelScore = QLabel("{}".format(values[rank]))
-            labelScore.setFont(QFont('Arial', 26))
-            wrapperScoreH.addWidget(labelName)
-            wrapperScoreH.addWidget(labelScore)
-            wrapperScoreH.addStretch(1)
-            wrapperScoreV.addLayout(wrapperScoreH)
-
-        vbox.addLayout(wrapperScoreV)
-        vbox.addStretch()
-        vbox.addLayout(hbox)
-        hbox.addWidget(backButton)
-
-        widget = QWidget()
-
-        # set the background image as the scores page
-        background = QLabel(widget)
-        background.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-        background.setStyleSheet(
-            "background-image: url(./assets/backgrounds/fond score.PNG)")
-
-        widget.setLayout(vbox)
-        self.setCentralWidget(widget)
-
     def gamePage(self):
+
+        global brain
+        global last_reward
+        global scores
+
         # add buttons for game page
-        hitButton = QPushButton("Hit")
-        hitButton.setStyleSheet(buttonStyle)
-        hitButton.clicked.connect(self.hitEvent)
-
-        standButton = QPushButton("Stand")
-        standButton.setStyleSheet(buttonStyle)
-        standButton.clicked.connect(self.standEvent)
-
-        splitButton = QPushButton("Split")
-        splitButton.setStyleSheet(buttonStyle)
-        splitButton.clicked.connect(self.splitEvent)
-
         leaveButton = QPushButton("Leave")
         leaveButton.setStyleSheet(buttonStyle)
         leaveButton.clicked.connect(self.leavePage)
@@ -181,10 +140,31 @@ class MainWindow(QMainWindow):
         labelDealer.setFont(QFont('Arial', 26))
         labelDealer.setStyleSheet(
             "padding:5px 20px; color: white; background-color:rgb(43, 43, 43); border-radius:20px")
+
+        # add number of dealer victory
+        DealerWin = QLabel("Victory: {}".format(self.game.scores['dealer']))
+        DealerWin.setFont(QFont('Arial', 26))
+        DealerWin.setStyleSheet(
+            "padding:5px 20px; color: white; background-color:rgb(43, 43, 43); border-radius:20px")
+
+        # add player name
         labelPlayer = QLabel("You: {}".format(
             self.game.playerHand.handValue()))
         labelPlayer.setFont(QFont('Arial', 26))
         labelPlayer.setStyleSheet(
+            "padding:5px 20px; color: white; background-color:rgb(43, 43, 43); border-radius:20px")
+
+        # add number of player victory
+        PlayerWin = QLabel("Victory: {} ({:.2f})".format(
+            self.game.scores['player'], self.game.scores['player'] / (self.game.scores['player'] + self.game.scores['dealer'] + 2e-16)))
+        PlayerWin.setFont(QFont('Arial', 26))
+        PlayerWin.setStyleSheet(
+            "padding:5px 20px; color: white; background-color:rgb(43, 43, 43); border-radius:20px")
+
+        # add ai score
+        AIScore = QLabel("AI Score: {:.3f}".format(brain.score()))
+        AIScore.setFont(QFont('Arial', 26))
+        AIScore.setStyleSheet(
             "padding:5px 20px; color: white; background-color:rgb(43, 43, 43); border-radius:20px")
 
         # layout
@@ -195,85 +175,151 @@ class MainWindow(QMainWindow):
 
         # add widgets
         wrapperGameV = QVBoxLayout()
+        wrapperGameH = QHBoxLayout()
+        wrapperGameH2 = QHBoxLayout()
 
         # label Dealer
-        wrapperGameV.addWidget(labelDealer, alignment=Qt.AlignCenter)
+        wrapperGameH.addWidget(labelDealer, alignment=Qt.AlignCenter)
+        wrapperGameH.addWidget(DealerWin, alignment=Qt.AlignCenter)
+        wrapperGameV.addLayout(wrapperGameH)
 
         # label player
         wrapperGameV.addStretch()
-        wrapperGameV.addWidget(labelPlayer, alignment=Qt.AlignCenter)
+        wrapperGameH2.addWidget(labelPlayer, alignment=Qt.AlignCenter)
+        wrapperGameH2.addWidget(PlayerWin, alignment=Qt.AlignCenter)
+        wrapperGameH2.addWidget(AIScore, alignment=Qt.AlignCenter)
+        wrapperGameV.addLayout(wrapperGameH2)
 
         vbox.addLayout(wrapperGameV)
         vbox.addLayout(hbox)
 
-        hbox.addWidget(hitButton)
-        hbox.addWidget(standButton)
-        # hbox.addWidget(splitButton)
         hbox.addWidget(leaveButton)
-
-        vbox.addLayout(wrapperGameV)
 
         widget = self.updateGamePage()
 
         widget.setLayout(vbox)
         self.setCentralWidget(widget)
 
+        return
+
+    def playHand(self):
+        if self.cardsDealt:
+
+            win = self.checkWin()
+
+            if win:
+                self.game.resetHands()
+                self.status = 0
+                return 1
+
+            playerState = self.game.playerHand.handValue()
+
+            dealerState = self.game.dealerHand.handValue()
+
+            AceState = 0
+            for card in self.game.playerHand.hand:
+                if card.getRank() == 1 and playerState + 10 <= 21:
+                    AceState = 1
+
+            last_signal = (
+                (np.array([playerState, dealerState, AceState])).T).tolist()
+            action2take = brain.update(last_reward, last_signal)
+            scores.append(brain.score())
+
+            if action2take == 0:
+                self.hitEvent()
+            else:
+                self.standEvent()
+
+    def checkWin(self):
+        global last_reward
+
         if self.game.dealerHand.handValue() == 21 and len(self.game.dealerHand.hand) == 2:
             loop = QEventLoop()
-            QTimer.singleShot(1000, loop.quit)
+            QTimer.singleShot(100, loop.quit)
             loop.exec_()
-            self.lostPage()
-            return
+            self.cardsDealt = False
+            last_reward = -1
+            self.lost()
+            return 1
 
         if self.game.playerHand.handValue() == 21 and len(self.game.playerHand.hand) == 2:
             loop = QEventLoop()
-            QTimer.singleShot(1000, loop.quit)
+            QTimer.singleShot(100, loop.quit)
             loop.exec_()
-            self.winPage()
-            return
+            self.cardsDealt = False
+            last_reward = 1
+            self.win()
+            return 1
 
         if self.game.playerHand.handValue() > 21:
             loop = QEventLoop()
-            QTimer.singleShot(1000, loop.quit)
+            QTimer.singleShot(100, loop.quit)
             loop.exec_()
-            self.lostPage()
-            return
+            self.cardsDealt = False
+            last_reward = -1
+            self.lost()
+            return 1
 
         if self.game.dealerHand.handValue() > 21:
             loop = QEventLoop()
-            QTimer.singleShot(1000, loop.quit)
+            QTimer.singleShot(100, loop.quit)
             loop.exec_()
-            self.winPage()
-            return
+            self.cardsDealt = False
+            last_reward = 1
+            self.win()
+            return 1
 
         if self.status == 1:
             if self.game.dealerHand.handValue() >= self.game.playerHand.handValue():
                 loop = QEventLoop()
-                QTimer.singleShot(1000, loop.quit)
+                QTimer.singleShot(100, loop.quit)
                 loop.exec_()
-                self.lostPage()
+                self.cardsDealt = False
+                last_reward = -1
+                self.lost()
+                return 1
+
+    def handleGame(self):
+        while not self.exitGame:
+            self.processGame()
 
     def processGame(self):
-        self.gamePage()
+        if not self.exitGame:
+            self.gamePage()
 
         # game
         loop = QEventLoop()
-        QTimer.singleShot(500, loop.quit)
+        QTimer.singleShot(100, loop.quit)
         loop.exec_()
         self.game.dealerTurn()
-        self.gamePage()
+        if not self.exitGame:
+            self.gamePage()
 
         loop = QEventLoop()
-        QTimer.singleShot(500, loop.quit)
+        QTimer.singleShot(100, loop.quit)
         loop.exec_()
         self.game.playerTurn()
-        self.gamePage()
+        if not self.exitGame:
+            self.gamePage()
 
         loop = QEventLoop()
-        QTimer.singleShot(500, loop.quit)
+        QTimer.singleShot(100, loop.quit)
         loop.exec_()
         self.game.playerTurn()
-        self.gamePage()
+        if not self.exitGame:
+            self.gamePage()
+
+        loop = QEventLoop()
+        QTimer.singleShot(100, loop.quit)
+        loop.exec_()
+        self.cardsDealt = True
+        if not self.exitGame:
+            self.gamePage()
+
+        stop = 0
+        while not stop:
+            stop = self.playHand()
 
     def updateGamePage(self):
         widget = QWidget()
@@ -306,120 +352,39 @@ class MainWindow(QMainWindow):
 
     def hitEvent(self):
         self.game.playerTurn()
-        self.gamePage()
-
-    def standEvent(self):
-        while self.game.dealerHand.handValue() <= self.game.playerHand.handValue():
-            loop = QEventLoop()
-            QTimer.singleShot(500, loop.quit)
-            loop.exec_()
-            self.game.dealerTurn()
+        if not self.exitGame:
             self.gamePage()
 
+    def standEvent(self):
+        while self.game.dealerHand.handValue() <= self.game.playerHand.handValue() or self.game.dealerHand.handValue() <= 17:
+            loop = QEventLoop()
+            QTimer.singleShot(100, loop.quit)
+            loop.exec_()
+            self.game.dealerTurn()
+            if not self.exitGame:
+                self.gamePage()
+
         self.status = 1
-        self.gamePage()
 
-    def splitEvent(self):
-        pass
-
-    def winPage(self):
-        # add buttons for win page
-        continueButton = QPushButton("Continue")
-        continueButton.setStyleSheet(buttonStyle)
-        continueButton.clicked.connect(self.newGame)
-
-        leaveButton = QPushButton("Leave")
-        leaveButton.setStyleSheet(buttonStyle)
-        leaveButton.clicked.connect(self.leavePage)
-
-        # layout
-        hbox = QHBoxLayout()
-        vbox = QVBoxLayout()
-
-        hbox.addStretch(1)
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-
-        hbox.addWidget(continueButton)
-        hbox.addWidget(leaveButton)
-
-        widget = QWidget()
-
-        # set the background image for leaving the game
-        background = QLabel(widget)
-        background.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-        background.setStyleSheet(
-            "background-image: url(./assets/backgrounds/fond gagne.PNG)")
-
-        widget.setLayout(vbox)
-        self.setCentralWidget(widget)
-
+    def win(self):
         self.game.updateScorePlayer()
 
-    def lostPage(self):
-        # add buttons for win page
-        continueButton = QPushButton("Continue")
-        continueButton.setStyleSheet(buttonStyle)
-        continueButton.clicked.connect(self.newGame)
-
-        leaveButton = QPushButton("Leave")
-        leaveButton.setStyleSheet(buttonStyle)
-        leaveButton.clicked.connect(self.leavePage)
-
-        # layout
-        hbox = QHBoxLayout()
-        vbox = QVBoxLayout()
-
-        hbox.addStretch(1)
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-
-        hbox.addWidget(continueButton)
-        hbox.addWidget(leaveButton)
-
-        widget = QWidget()
-
-        # set the background image for leaving the game
-        background = QLabel(widget)
-        background.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-        if self.game.dealerHand.handValue() == 21 and len(self.game.dealerHand.hand) == 2:
-            background.setStyleSheet(
-                "background-image: url(./assets/backgrounds/fond perdu blackjack.PNG)")
-        else:
-            background.setStyleSheet(
-                "background-image: url(./assets/backgrounds/fond perdu.PNG)")
-
-        widget.setLayout(vbox)
-        self.setCentralWidget(widget)
-
+    def lost(self):
         self.game.updateScoreDealer()
-
-    def newGame(self):
-        self.game.resetHands()
-        self.status = 0
-        self.processGame()
 
     def leavePage(self):
         # add buttons for game page
-        confirmButton = QPushButton("Confirm")
+        self.exitGame = True
+        saveButton = QPushButton("Save Model")
+        confirmButton = QPushButton("Confirm Leave")
+        saveButton.setStyleSheet(buttonStyle)
         confirmButton.setStyleSheet(buttonStyle)
 
-        # add widgets
-        label = QLabel("Enter your name:")
-        label.setFont(QFont('Arial', 26))
-
-        self.lineEdit.setStyleSheet(
-            "padding:5px 10px")
-
-        # layout
-        wrapperLE = QVBoxLayout()
-        wrapperLE.setContentsMargins(500, 50, 250, 650)
-        wrapperLE.addStretch()
-        wrapperLE.addWidget(label, alignment=Qt.AlignTop)
-        wrapperLE.addWidget(self.lineEdit, alignment=Qt.AlignTop)
-        wrapperLE.addWidget(confirmButton, alignment=Qt.AlignTop)
-
         widget = QWidget()
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(saveButton, alignment=Qt.AlignBottom)
+        hbox.addWidget(confirmButton, alignment=Qt.AlignBottom)
 
         # set the background image for leaving the game
         background = QLabel(widget)
@@ -427,18 +392,22 @@ class MainWindow(QMainWindow):
         background.setStyleSheet(
             "background-image: url(./assets/backgrounds/fond au revoir.PNG)")
 
-        widget.setLayout(wrapperLE)
+        widget.setLayout(hbox)
         self.setCentralWidget(widget)
 
-        confirmButton.clicked.connect(self.registerUser)
+        saveButton.clicked.connect(self.saveModel)
+        confirmButton.clicked.connect(sys.exit)
 
-    def registerUser(self):
-        if self.lineEdit.text() != "":
-            self.username = self.lineEdit.text()
-
-        self.game.scores[self.username] = self.game.scores['player']
-        self.game.saveGameScore()
+    def saveModel(self):
+        brain.save(self.game.scores['dealer'],
+                   self.game.scores['player'])
         sys.exit()
+
+    def loadModel(self):
+        brain.load()
+        self.game.scores['dealer'] = brain.last_vicDealer
+        self.game.scores['player'] = brain.last_vicPlayer
+        self.handleGame()
 
 
 if __name__ == "__main__":
